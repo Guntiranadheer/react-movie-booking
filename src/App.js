409d7ccb77f6efa9fe006af4d9a43f7c524ee9a5
Ranadheer <guntiranadheer11@gmail.com>
2025-01-26 20:04:09 +0530

    react practice

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='container movie-card'>
+  <div className='movie'>
+      <h1> MOVIE NAMES:</h1>
+      <h4>movie name1:Sankrathi ki vasthunam</h4>
+      <p> show Time: 10AM</p>
+  </div>
+  <div className='movie'>
+    <h4>Movie name2: Daku maharaj</h4>
+    <p> show time: 12AM</p>
+  </div>
+  <div className='movie'>
+    <h4> Movie name3:Pushpa-2</h4>
+    <p> show time:6PM</p>
+  </div>
+  </div>
+  );
+}
+
+export default App;
